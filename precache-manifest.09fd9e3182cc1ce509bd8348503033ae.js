eb2988da9747c22c72fb7c15b5431af991b898c8
Damian Rogulski <damian.rogulski@netinteractive.pl>
2019-12-02 17:37:50 +0100

    Updates

---
+++ precache-manifest.09fd9e3182cc1ce509bd8348503033ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3779a3be6062bda13a5955b9a68acd9",
+    "url": "/GH-App/index.html"
+  },
+  {
+    "revision": "fa8d44b32bd2b2eac32c",
+    "url": "/GH-App/static/js/2.a1b35a73.chunk.js"
+  },
+  {
+    "revision": "30e6974812848689bf80",
+    "url": "/GH-App/static/js/main.ae8d128d.chunk.js"
+  },
+  {
+    "revision": "3b697825dd2cb80d9a60",
+    "url": "/GH-App/static/js/runtime-main.94ca375a.js"
+  },
+  {
+    "revision": "9c0790b969781c96797c69a4069c6446",
+    "url": "/GH-App/static/media/arrow.9c0790b9.svg"
+  }
+]);
